78a4c2693c5a221f5232a0d4a501455b4d926b02
soumitrakumarmund <soumitramund93@.gmai.com>
2019-08-25 10:11:24 +0530

    Add If check

--- src/main/java/com/gtihib/project/git_demo/App.java
+++ src/main/java/com/gtihib/project/git_demo/App.java
@@ -10,6 +10,10 @@ public class App
 	public void addData(String data)
 	{
 		System.out.println("Data "+data);
+		if(data.equalsIgnoreCase("Test"))
+		{
+			System.out.println("Test Pass");
+		}
 	}
     public static void main( String[] args )
     {
