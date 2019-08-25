66c6b2b6d4255084ef4ac0ef40c827d3067b997d
soumitrakumarmund <soumitramund93@.gmai.com>
2019-08-25 10:06:59 +0530

    Add project to repo

---
+++ src/main/java/com/gtihib/project/git_demo/App.java
@@ -0,0 +1,18 @@
+package com.gtihib.project.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String data)
+	{
+		System.out.println("Data "+data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
